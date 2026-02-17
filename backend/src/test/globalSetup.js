31b435a7914228b0878736250ccafb565c45cec3
checonunez74 <checonunez@gmail.com>
2026-02-17 17:29:04 -0600

    Fix CI: use MongoDB 8.0 for mongodb-memory-server

--- backend/src/test/globalSetup.js
+++ backend/src/test/globalSetup.js
@@ -2,9 +2,7 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 
 export default async function globalSetup() {
   const mongod = await MongoMemoryServer.create({
-    binary: {
-      version: '6.0.6',
-    },
+    binary: { version: '8.0.0' },
   })
 
   global.__MONGOD__ = mongod
